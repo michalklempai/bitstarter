830b2d3a3169702b9c08bd8bb8531b5ec5872e78
Michal Klempai <michal.klempai@gmail.com>
2013-08-04 16:14:02 +0000

    Express installed

--- web.js
+++ web.js
@@ -1,14 +1,16 @@
 var fs = require('fs'); 
 var express = require('express');
+var htmlFile = 'index.html';
 var app = express();
+
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  var content = fs.readFileSync('index.html');
+  var content = fs.readFileSync(htmlFile);
   response.send(content.toString());
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
